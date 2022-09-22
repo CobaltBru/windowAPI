e5a530bef40543c20481ebb9560d62fda437cbe9
CobaltBru <cobaltbru123@gmail.com>
2022-09-23 00:21:05 +0900

    .

---
+++ ch03/ch03_4/소스.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <TCHAR.H>
+#include <stdio.h>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASS	WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);		//윈도우 아이콘
+	WndClass.hCursor = LoadCursor(NULL, IDC_IBEAM);		//커서 모양
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경 색
+	WndClass.lpszMenuName = NULL;	//메뉴 이름
+	WndClass.lpszClassName = _T("Window Class Name");	//윈도우 클래스 이름
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Cobaltbru's First Window"),		//윈도우 타이틀 이름
+		WS_OVERLAPPEDWINDOW,		//윈도우 스타일
+		200,	//윈도우 위치 X
+		300,	//윈도우 위치 Y
+		600,	//윈도우 가로
+		400,	//윈도우 세로
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);		//윈도우 기본 출력 함수
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC				hdc;
+	PAINTSTRUCT		ps;
+	static int		x, y;
+	static RECT		rectView;
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		GetClientRect(hwnd, &rectView);
+		x = 20;
+		y = 20;
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_RIGHT)
+		{
+			SetTimer(hwnd, 1, 70, NULL);
+		}
+		break;
+	case WM_TIMER:
+		x += 40;
+		if (x + 20 > rectView.right) x -= 40;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+	case WM_DESTROY:
+		KillTimer(hwnd, 1);
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
---
+++ ch03/ch03_5/소스.cpp
@@ -0,0 +1,56 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <TCHAR.H>
+#include <math.h>
+#define BSIZE 40
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASS	WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);		//윈도우 아이콘
+	WndClass.hCursor = LoadCursor(NULL, IDC_IBEAM);		//커서 모양
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경 색
+	WndClass.lpszMenuName = NULL;	//메뉴 이름
+	WndClass.lpszClassName = _T("Window Class Name");	//윈도우 클래스 이름
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Cobaltbru's First Window"),		//윈도우 타이틀 이름
+		WS_OVERLAPPEDWINDOW,		//윈도우 스타일
+		200,	//윈도우 위치 X
+		300,	//윈도우 위치 Y
+		600,	//윈도우 가로
+		400,	//윈도우 세로
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);		//윈도우 기본 출력 함수
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+double LengthPts(int x1, int y1, int x2, int y2)
+{
+	return(sqrt((float)((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))));
+}
+
+BOOL InCircle(int x, int y, int mx, int my)
+{
+	if (LengthPts(x, y, mx, my) < BSIZE) return TRUE;
+	else return FALSE;
+}
