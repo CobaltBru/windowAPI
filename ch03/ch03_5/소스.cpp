634a683665f65d2729e4486837e88d3ae56b0d49
CobaltBru <cobaltbru123@gmail.com>
2022-09-24 00:56:40 +0900

    .

--- ch03/ch03_5/소스.cpp
+++ ch03/ch03_5/소스.cpp
@@ -17,7 +17,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	WndClass.cbWndExtra = 0;
 	WndClass.hInstance = hInstance;
 	WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);		//윈도우 아이콘
-	WndClass.hCursor = LoadCursor(NULL, IDC_IBEAM);		//커서 모양
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);		//커서 모양
 	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경 색
 	WndClass.lpszMenuName = NULL;	//메뉴 이름
 	WndClass.lpszClassName = _T("Window Class Name");	//윈도우 클래스 이름
@@ -53,4 +53,52 @@ BOOL InCircle(int x, int y, int mx, int my)
 {
 	if (LengthPts(x, y, mx, my) < BSIZE) return TRUE;
 	else return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC				hdc;
+	PAINTSTRUCT		ps;
+	static int		x, y;
+	static BOOL		Selection;
+	int				mx, my;
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		x = 50; y = 50;
+		Selection = FALSE;
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		if (Selection)
+			Rectangle(hdc, x - BSIZE, y - BSIZE, x + BSIZE, y + BSIZE);
+		Ellipse(hdc, x - BSIZE, y - BSIZE, x + BSIZE, y + BSIZE);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_LBUTTONDOWN:
+		mx = LOWORD(lParam);
+		my = HIWORD(lParam);
+		if (InCircle(x, y, mx, my)) Selection = TRUE;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+	case WM_LBUTTONUP:
+		Selection = FALSE;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+	case WM_MOUSEMOVE:
+		mx = LOWORD(lParam);
+		my = HIWORD(lParam);
+		if (Selection)
+		{
+			x = mx;
+			y = my;
+			InvalidateRgn(hwnd, NULL, TRUE);
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
---
+++ ch03/ch03_7/소스.cpp
@@ -0,0 +1,100 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <TCHAR.H>
+#include <math.h>
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASS	WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);		//윈도우 아이콘
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);		//커서 모양
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경 색
+	WndClass.lpszMenuName = NULL;	//메뉴 이름
+	WndClass.lpszClassName = _T("Window Class Name");	//윈도우 클래스 이름
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Cobaltbru's First Window"),		//윈도우 타이틀 이름
+		WS_OVERLAPPEDWINDOW,		//윈도우 스타일
+		200,	//윈도우 위치 X
+		300,	//윈도우 위치 Y
+		600,	//윈도우 가로
+		400,	//윈도우 세로
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);		//윈도우 기본 출력 함수
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC			hdc;
+	PAINTSTRUCT	ps;
+	static int	startX, startY, oldX, oldY;
+	static BOOL	Drag;
+	int			endX, endY;
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		Drag = FALSE;
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		MoveToEx(hdc, startX, startY, NULL);
+		LineTo(hdc, oldX, oldY);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_LBUTTONDOWN:
+		startX = oldX = LOWORD(lParam);
+		startY = oldY = HIWORD(lParam);
+		Drag = TRUE;
+		break;
+	case WM_LBUTTONUP:
+		startX = oldX = 0;
+		startY = oldY = 0;
+		Drag = FALSE;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+	case WM_MOUSEMOVE:
+		hdc = GetDC(hwnd);
+		if (Drag)
+		{
+			SetROP2(hdc, R2_XORPEN);
+			SelectObject(hdc, (HPEN)GetStockObject(WHITE_PEN));
+			endX = LOWORD(lParam);
+			endY = HIWORD(lParam);
+			MoveToEx(hdc, startX, startY, NULL);
+			LineTo(hdc, oldX, oldY);
+			MoveToEx(hdc, startX, startY, NULL);
+			LineTo(hdc, endX, endY);
+			oldX = endX; oldY = endY;
+		}
+		ReleaseDC(hwnd, hdc);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+	
+
+
+}
