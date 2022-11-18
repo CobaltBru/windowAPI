d1d0b224d1dfb1c5169dafffd19bad25ef05bd61
CobalrBru <cobaltbru123@gmail.com>
2022-11-18 13:36:17 +0900

    .

---
+++ ch05/ch05_2/소스.cpp
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "resource.h"
+#include <windows.h>
+#include <TCHAR.H>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define BSIZE 40
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+HINSTANCE hInst;
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND		hwnd;
+	MSG			msg;
+	WNDCLASS	WndClass;
+	hInst = hInstance;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_QUESTION);		//윈도우 아이콘
+	WndClass.hCursor = LoadCursor(NULL, IDC_IBEAM);		//커서 모양
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);	//배경 색
+	//WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU4_PRAC_3);	//메뉴 이름
+	WndClass.lpszClassName = _T("Window Class Name");	//윈도우 클래스 이름
+	RegisterClass(&WndClass);
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("Cobaltbru's First Window"),		//윈도우 타이틀 이름
+		WS_OVERLAPPEDWINDOW,		//윈도우 스타일
+		0,	//윈도우 위치 X
+		0,	//윈도우 위치 Y
+		1200,	//윈도우 가로
+		1000,	//윈도우 세로
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);		//윈도우 기본 출력 함수
+	UpdateWindow(hwnd);
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc, memdc;
+	PAINTSTRUCT ps;
+	static HBITMAP hBitmap;
+
+	switch (iMsg)
+	{
+	case WM_CREATE:
+		hBitmap = (HBITMAP)LoadBitmap(hInst, MAKEINTRESOURCE(IDB_BITMAP5_2));
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		memdc = CreateCompatibleDC(hdc);
+		SelectObject(memdc, hBitmap);
+		BitBlt(hdc, 0, 0, 1000, 900, memdc, 0, 0, SRCCOPY);
+		DeleteDC(memdc);
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
